6bfddbdd2665e7add824f3c7cb61c02a49ac8c04
alfredebi9 <alfredsalvadorfav@gmail.com>
2024-05-13 13:51:55 +0100

    backend login and routes added

---
+++ public/js/client.js
@@ -0,0 +1,13 @@
+
+// fetch('/login', {
+//   method: 'POST',
+//   headers: new Headers({
+//     'Content-Type': 'application/json'
+//   }),
+// })
+//   .then(res => res.json())
+//   .then(data => {
+//     console.log(data);
+//     // alert(message)
+//     document.getElementById('message-container').textContent = data;
+//   });
--- public/js/index.js
+++ public/js/index.js
@@ -20,3 +20,11 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   })
 })
+
+
+  function logout() {
+    document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    document.cookie = 'username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    window.location.href = '/logout';
+  }
+
